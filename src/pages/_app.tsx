addf801ce950ece65afb2e841b529d3d342cb2b6
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-10-16 00:20:47 +0900

    dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,9 +3,21 @@ import { type AppType } from "next/app";
 import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
+import { ThemeProvider } from "@/components/themeprovider";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
