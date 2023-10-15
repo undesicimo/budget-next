5b2d88575cf1745e6b6320e7ecb1229cdacfdabb
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-10-16 00:13:10 +0900

    lint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,7 +21,7 @@ const config = {
         fixStyle: "inline-type-imports",
       },
     ],
-    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/no-misused-promises": "off",
   },
 };
