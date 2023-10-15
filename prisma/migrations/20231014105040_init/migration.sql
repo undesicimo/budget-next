173405a420dbd6ff5fd5baff0fa5f3e528ef174f
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-10-15 10:16:23 +0900

    prisma

---
+++ prisma/migrations/20231014105040_init/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "Example" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Example_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Example_name_idx" ON "Example"("name");
