0f90535c1628e53a81847ba3d3842f7beab1fb34
undesicimo <105863111+undesicimo@users.noreply.github.com>
2023-11-01 13:43:33 +0900

    constants

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from "@playwright/test";
-import { getDate } from "./helpers/date";
+import { SCREENSHOT_PATH } from "./common/constants";
 
 test("has title", async ({ page, baseURL }) => {
   await page.goto(`${baseURL}`);
 
-  await page.screenshot({ path: `test-results/${getDate()}.png` });
+  await page.screenshot({ path: SCREENSHOT_PATH });
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Budget/);
 });
